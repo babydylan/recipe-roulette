088d46e89d9348943f3499f51128bab605566319
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-09-24 13:38:32 -0700

    included event listener for submit button, results are hidden and will appear once button is clicked

--- script.js
+++ script.js
@@ -2,25 +2,32 @@
 //     M.updateTextFields();
 //   });
 
-
-$ (function() {
-
+  function fetchCuisineRecipe() {
 
 
-    function fetchCuisineRecipe () {
-  
-    
-  var apiKey = "ff436b868e184aa8b285a0654110973b";
-  var spoonacularUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apiKey + "&cuisine=japanese";
-  
-  $.ajax({
+    var apiKey = "ff436b868e184aa8b285a0654110973b";
+    var spoonacularUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apiKey + "&cuisine=japanese";
+
+    $.ajax({
       url: spoonacularUrl,
       method: "GET",
     }).then(function (data) {
       console.log(data);
-  });
+    });
 
   }
 
+
+
+
+$("section").hide();
+
+$("#submitRequest").on("click",function(event){
+
+  event.preventDefault();
+  $("section").fadeIn(1000);
   fetchCuisineRecipe();
-  });
+
+})
+
+
