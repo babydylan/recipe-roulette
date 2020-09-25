1dc6924a4223d12f0a05a520286bbb9762ae04ba
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-09-25 00:20:54 -0700

    input variable for user created, fetchCuisineRecipe modified to allow slider effect on images, html file updated ul for recipes

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // $(document).ready(function() {
 //     M.updateTextFields();
 //   });
+
+// Hides recipe results until user clicks submit 
   $("section").hide();
 
    
-  // var userIngredientInput = userIngredientInput.toLowerCase();
-  
+  // function below builds URL to retrieve recipe data
+
   function fetchCuisineRecipe() {
-    
+    // query variable obtains user input
     var query= $("#ingredient_1")
       .val()
       .toLowerCase()
@@ -26,21 +28,36 @@
       var newDiv = $(".box");
 
       newDiv.text(title);
+
+      // For loop below creates a new li, h3, and img tag for each available recipe
       for (var i = 0; i < data.results.length; i++) {
         var title = data.results[i].title;
-        var newDisplay = $("<div>");
+        var newDisplay = $("<li>");
         var newTitle = $("<h3>")
+          .addClass("caption center-align")
+          .addClass("orange-text accent-3");
         newTitle.text(title);
         newTitle.appendTo(newDisplay);
+        // Line 36-42 work with images
         newDisplay.appendTo(newDiv);
         var imgLink = data.results[i].image;
         var newImg = $("<img>").attr("src", imgLink);
+
+        // each li,h3,and img tag becomes appended to a ul tag where a slider class is used 
         newImg.appendTo(newDisplay);
-        var idLink = $("<button>").text("Click Me For Recipe!");
+        // button is attached to images using caption class
+        var idLink = $("<button>")
+        .addClass("btn-small")
+        .addClass("caption bottom")
+        .text("Click Me For Recipe!");
         idLink.attr("id", data.results[i].id)
         idLink.appendTo(newDisplay);
+        
       };
+      // slider call below allows for recipe images to appear one at a time 
+      $('.slider').slider();
 
+      // Event listener for button located on image which redirects to a new page 
       $("button").on("click", function(){
         var idNum = $(this).attr("id");
         var getRecipeUrl = "https://api.spoonacular.com/recipes/" + idNum + "/information?apiKey=" + apiKey;
@@ -60,11 +77,12 @@
   };
 
 
-
+// Event listener on submit button 
   $("#submitRequest").on("click",function(event){
 
     event.preventDefault();
     $("section").fadeIn(1000);
+   
     fetchCuisineRecipe();
   
   });
