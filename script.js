2bfa1598c6868be421582bddf36eca3703629342
Dylan Nguyen <babydylannguyen@gmail.com>
2020-09-25 11:54:20 -0700

    add cuisine var

--- script.js
+++ script.js
@@ -3,105 +3,107 @@
 //   });
 
 // Hides recipe results until user clicks submit 
-  function hide(){
-    $("section").hide();
-  };
-  hide();
+function hide(){
+  $("section").hide();
+};
+hide();
 
-  // cuisine type value
+// cuisine type value
 
-  $("li").on("click", function(){
-    
-    var userCuisineType = $(this).text();
-    var dropDownarrow = $("<i>" + "expand_more" + "</i>").addClass("material-icons right");
-    $(".dropdown-button")
-    .text(userCuisineType)
-    .append(dropDownarrow);
-
-  });
+$("li").on("click", function(){
   
+  var userCuisineType = $(this).text();
+  cuisine = userCuisineType;
+  var dropDownarrow = $("<i>" + "expand_more" + "</i>").addClass("material-icons right");
+  $(".dropdown-button")
+  .text(userCuisineType)
+  .append(dropDownarrow);
+
+});
+
+var cuisine = "";
+
+
+ 
+// function below builds URL to retrieve recipe data
+
+function fetchCuisineRecipe() {
+  // query variable obtains user input
+  var query= $("#ingredient_1")
+    .val()
+    .toLowerCase()
+    .trim();
+
+  var apiKey = "8a9e2ee45fae41ecb210b7de4e23a7b1";
   
+  var spoonacularUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apiKey + "&query=" + query + "&cuisine=" + cuisine;
+  console.log(spoonacularUrl);
 
+  $.ajax({
+    url: spoonacularUrl,
+    method: "GET",
+  }).then(function (data) {
+    console.log(data);
+    var newDiv = $(".box");
 
-   
-  // function below builds URL to retrieve recipe data
+    newDiv.text(title);
 
-  function fetchCuisineRecipe() {
-    // query variable obtains user input
-    var query= $("#ingredient_1")
-      .val()
-      .toLowerCase()
-      .trim();
+    // For loop below creates a new li, h3, and img tag for each available recipe
+    for (var i = 0; i < data.results.length; i++) {
+      var title = data.results[i].title;
+      var newDisplay = $("<li>");
+      var newTitle = $("<h3>")
+        .addClass("caption center-align")
+        .addClass("orange-text accent-3");
+      newTitle.text(title);
+      newTitle.appendTo(newDisplay);
+      // Line 36-42 work with images
+      newDisplay.appendTo(newDiv);
+      var imgLink = data.results[i].image;
+      var newImg = $("<img>").attr("src", imgLink);
 
-    var apiKey = "8a9e2ee45fae41ecb210b7de4e23a7b1";
-    
-    var spoonacularUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apiKey + "&query=" + query;
-    console.log(spoonacularUrl);
+      // each li,h3,and img tag becomes appended to a ul tag where a slider class is used 
+      newImg.appendTo(newDisplay);
+      // button is attached to images using caption class
+      var idLink = $("<button>")
+      .addClass("btn-small")
+      .addClass("caption bottom")
+      .text("Click Me For Recipe!");
+      idLink.attr("id", data.results[i].id)
+      idLink.appendTo(newDisplay);
+      
+    };
+    // slider call below allows for recipe images to appear one at a time 
+    $('.slider').slider();
 
-    $.ajax({
-      url: spoonacularUrl,
-      method: "GET",
-    }).then(function (data) {
-      console.log(data);
-      var newDiv = $(".box");
-
-      newDiv.text(title);
-
-      // For loop below creates a new li, h3, and img tag for each available recipe
-      for (var i = 0; i < data.results.length; i++) {
-        var title = data.results[i].title;
-        var newDisplay = $("<li>");
-        var newTitle = $("<h3>")
-          .addClass("caption center-align")
-          .addClass("orange-text accent-3");
-        newTitle.text(title);
-        newTitle.appendTo(newDisplay);
-        // Line 36-42 work with images
-        newDisplay.appendTo(newDiv);
-        var imgLink = data.results[i].image;
-        var newImg = $("<img>").attr("src", imgLink);
-
-        // each li,h3,and img tag becomes appended to a ul tag where a slider class is used 
-        newImg.appendTo(newDisplay);
-        // button is attached to images using caption class
-        var idLink = $("<button>")
-        .addClass("btn-small")
-        .addClass("caption bottom")
-        .text("Click Me For Recipe!");
-        idLink.attr("id", data.results[i].id)
-        idLink.appendTo(newDisplay);
-        
-      };
-      // slider call below allows for recipe images to appear one at a time 
-      $('.slider').slider();
-
-      // Event listener for button located on image which redirects to a new page 
-      $("button").on("click", function(){
-        var idNum = $(this).attr("id");
-        var getRecipeUrl = "https://api.spoonacular.com/recipes/" + idNum + "/information?apiKey=" + apiKey;
-        $.ajax({
-          url: getRecipeUrl,
-          method: "GET",
-        }).then(function (data) {
-          var spoonRecipe = data.spoonacularSourceUrl;
-          window.location.replace(spoonRecipe);
-        })
-
-      });
-
+    // Event listener for button located on image which redirects to a new page 
+    $("button").on("click", function(){
+      var idNum = $(this).attr("id");
+      var getRecipeUrl = "https://api.spoonacular.com/recipes/" + idNum + "/information?apiKey=" + apiKey;
+      $.ajax({
+        url: getRecipeUrl,
+        method: "GET",
+      }).then(function (data) {
+        var spoonRecipe = data.spoonacularSourceUrl;
+        window.location.replace(spoonRecipe);
+      })
 
     });
 
-  };
+
+  });
+
+};
 
 
 // Event listener on submit button 
-  $("#submitRequest").on("click",function(event){
+$("#submitRequest").on("click",function(event){
 
-    event.preventDefault();
-    $("section").fadeIn(1000);
-   
-    fetchCuisineRecipe();
-  
-  });
+  event.preventDefault();
+  $(".box").empty();
+  $("section").fadeIn(1000);
+ 
+  fetchCuisineRecipe();
+
+});
 
